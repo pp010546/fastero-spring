651aba526e76a7a51622863f314a00048a52c2f6
Philip <philip0105@gmail.com>
2022-10-07 17:24:23 +0800

    init

---
+++ src/main/java/tw/com/fasterospring/FasteroSpringApplication.java
@@ -0,0 +1,13 @@
+package tw.com.fasterospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasteroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasteroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/fasterospring/FasteroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.fasterospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasteroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
