addc4dff457d3f87af9b1dbed6f20a82086d69db
Philip <philip0105@gmail.com>
2022-10-18 17:44:07 +0800

    update Regex: email format

--- src/main/java/tw/com/fasterospring/common/Regex.java
+++ src/main/java/tw/com/fasterospring/common/Regex.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Regex {
 	
 	public static boolean emailCheck(String emailAddress) {
-	    return Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
+	    return Pattern.compile("^(?=.{6,40}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
 	            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
 	      .matcher(emailAddress)
 	      .matches();
