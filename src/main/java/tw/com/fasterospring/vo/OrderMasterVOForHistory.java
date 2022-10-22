fe5f7872c4637ca25dd064b16189425225fb0eb1
Philip <philip0105@gmail.com>
2022-10-22 19:34:08 +0800

    fix code, fix css

--- src/main/java/tw/com/fasterospring/vo/OrderMasterVOForHistory.java
+++ src/main/java/tw/com/fasterospring/vo/OrderMasterVOForHistory.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
